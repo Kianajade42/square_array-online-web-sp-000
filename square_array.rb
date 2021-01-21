5327215032a81af31b4b813569c67f1613a855de
Kiana Long <kianajade42.kl@gmail.com>
2021-01-21 16:11:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,5 @@
 def square_array(array)
-  numbers = [1,2,3]
- array.each do |element| numbers << element ** 2
- end
-end
+  squared = []
+  array.each { |element| squared << element ** 2 }
+  squared
+end 
